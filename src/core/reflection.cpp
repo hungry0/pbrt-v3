b8dd90e15d817531f08030b30106c41b3bb54e34
hungry0 <277862831@qq.com>
2020-10-16 11:15:10 +0800

    删除 OrenNayar

--- src/core/reflection.cpp
+++ src/core/reflection.cpp
@@ -135,35 +135,6 @@ std::string LambertianReflection::ToString() const {
            std::string(" ]");
 }
 
-Spectrum OrenNayar::f(const Vector3f &wo, const Vector3f &wi) const {
-    Float sinThetaI = SinTheta(wi);
-    Float sinThetaO = SinTheta(wo);
-    // Compute cosine term of Oren-Nayar model
-    Float maxCos = 0;
-    if (sinThetaI > 1e-4 && sinThetaO > 1e-4) {
-        Float sinPhiI = SinPhi(wi), cosPhiI = CosPhi(wi);
-        Float sinPhiO = SinPhi(wo), cosPhiO = CosPhi(wo);
-        Float dCos = cosPhiI * cosPhiO + sinPhiI * sinPhiO;
-        maxCos = std::max((Float)0, dCos);
-    }
-
-    // Compute sine and tangent terms of Oren-Nayar model
-    Float sinAlpha, tanBeta;
-    if (AbsCosTheta(wi) > AbsCosTheta(wo)) {
-        sinAlpha = sinThetaO;
-        tanBeta = sinThetaI / AbsCosTheta(wi);
-    } else {
-        sinAlpha = sinThetaI;
-        tanBeta = sinThetaO / AbsCosTheta(wo);
-    }
-    return R * InvPi * (A + B * maxCos * sinAlpha * tanBeta);
-}
-
-std::string OrenNayar::ToString() const {
-    return std::string("[ OrenNayar R: ") + R.ToString() +
-           StringPrintf(" A: %f B: %f ]", A, B);
-}
-
 Spectrum MicrofacetReflection::f(const Vector3f &wo, const Vector3f &wi) const {
     Float cosThetaO = AbsCosTheta(wo), cosThetaI = AbsCosTheta(wi);
     Vector3f wh = wi + wo;
